fc195cacf78e5eac634f7508ce5726719a1a5288
okechukwu promise <okechukwupromise@gmail.com>
2026-04-13 00:15:17 +0100

    try test

--- cgpa_cal/src/page/Application.jsx
+++ cgpa_cal/src/page/Application.jsx
@@ -17,7 +17,7 @@ export function Application() {
   });
   const [message, setmessage] = useState("")
  const [errmessage, seterrmessage] = useState("")
-  const API_URL = import.meta.env.VITE_API_URL || 
+  const API_URL = "https://edual-tracker.onrender.com "|| 
   "http://localhost:5000";
 
 
