925d28e39cc4fbf54aa3a34def4562effaa2e2da
okechukwu promise <okechukwupromise@gmail.com>
2026-04-12 13:10:25 +0100

    fix case-sensitive import for render

--- server/routes/gpa.js
+++ server/routes/gpa.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Gpa = require("../models/Gpa");
+const Gpa = require("../models/gpa");
 const auth = require("../middleware/auth");
 
 
